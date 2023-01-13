861fa4714bb5aedc39646f8a3392abe96095f5ad
joshblox <josh.thomas@bloxroute.com>
2023-01-13 10:14:56 -0600

    update diclaimer dialog text

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -223,37 +223,37 @@ function DisclaimerDialog() {
         <div className="grow text-sm leading-normal text-[#abc4ffb3] scrollbar-width-thin overflow-auto h-96 mobile:h-12 rounded p-4 my-6 mobile:my-4 bg-[#141041]">
           <p className="mb-3">
             This website-hosted user interface (this "Interface") is an open source frontend software portal to the
-            Raydium protocol, a decentralized and community-driven collection of blockchain-enabled smart contracts and
-            tools (the "Raydium Protocol"). This Interface and the Raydium Protocol are made available by the Raydium
-            Holding Foundation, however all transactions conducted on the protocol are run by related permissionless
-            smart contracts. As the Interface is open-sourced and the Raydium Protocol and its related smart contracts
-            are accessible by any user, entity or third party, there are a number of third party web and mobile
-            user-interfaces that allow for interaction with the Raydium Protocol.
+            bloXroute Solana Trader API, a high-performance tool to interact with Solana applications (the "bloXroute
+            Solana Trader API"). This Interface and the bloXroute Solana Trader API are made available by the bloXroute
+            Labs, however all transactions conducted on the protocol are run by related permissionless smart contracts.
+            As the Interface is open-sourced and the bloXroute Solana Trader API is accessible by any user, entity or
+            third party,and mobile user-interfaces that allow for interaction with the bloXroute Solana Trader API.
           </p>
           <p className="mb-3">
-            THIS INTERFACE AND THE RAYDIUM PROTOCOL ARE PROVIDED "AS IS", AT YOUR OWN RISK, AND WITHOUT WARRANTIES OF
-            ANY KIND. The Raydium Holding Foundation does not provide, own, or control the Raydium Protocol or any
-            transactions conducted on the protocol or via related smart contracts. By using or accessing this Interface
-            or the Raydium Protocol and related smart contracts, you agree that no developer or entity involved in
-            creating, deploying or maintaining this Interface or the Raydium Protocol will be liable for any claims or
-            damages whatsoever associated with your use, inability to use, or your interaction with other users of, this
-            Interface or the Raydium Protocol, including any direct, indirect, incidental, special, exemplary, punitive
-            or consequential damages, or loss of profits, digital assets, tokens, or anything else of value.
+            THIS INTERFACE AND THE BLOXROUTE SOLANA TRADER API ARE PROVIDED "AS IS", AT YOUR OWN RISK, AND WITHOUT
+            WARRANTIES OF ANY KIND. The bloXroute Labs does not provide, own, or control the Solana applications
+            interacted with or any transactions conducted on the blockchain or via related smart contracts. By using or
+            accessing this Interface or the bloXroute Solana Trader API and related smart contracts, you agree that no
+            developer or entity involved in creating, deploying or maintaining this Interface or the bloXroute Solana
+            Trader API will be liable for any claims or damages whatsoever associated with your use, inability to use,
+            or your interaction with other users of, this Interface or the bloXroute Solana Trader API, including any
+            direct, indirect, incidental, special, exemplary, punitive or consequential damages, or loss of profits,
+            digital assets, tokens, or anything else of value.
           </p>
           <p className="mb-3">
-            The Raydium Protocol is not available to residents of Belarus, the Central African Republic, The Democratic
-            Republic of Congo, the Democratic People's Republic of Korea, the Crimea, Donetsk People's Republic, and
-            Luhansk People's Republic regions of Ukraine, Cuba, Iran, Libya, Somalia, Sudan, South Sudan, Syria, the
-            USA, Yemen, Zimbabwe and any other jurisdiction in which accessing or using the Raydium Protocol is
-            prohibited (the "Prohibited Jurisdictions").
+            The bloXroute Solana Trader API is not available to residents of Belarus, the Central African Republic, The
+            Democratic Republic of Congo, the Democratic People's Republic of Korea, the Crimea, Donetsk People's
+            Republic, and Luhansk People's Republic regions of Ukraine, Cuba, Iran, Libya, Somalia, Sudan, South Sudan,
+            Syria, the USA, Yemen, Zimbabwe and any other jurisdiction in which accessing or using the bloXroute Solana
+            Trader API is prohibited (the "Prohibited Jurisdictions").
           </p>
           <p className="mb-3">
-            By using or accessing this Interface, the Raydium Protocol, or related smart contracts, you represent that
-            you are not located in, incorporated or established in, or a citizen or resident of the Prohibited
-            Jurisdictions. You also represent that you are not subject to sanctions or otherwise designated on any list
-            of prohibited or restricted parties or excluded or denied persons, including but not limited to the lists
-            maintained by the United States' Department of Treasury's Office of Foreign Assets Control, the United
-            Nations Security Council, the European Union or its Member States, or any other government authority.
+            By using or accessing this Interface, the bloXroute Solana Trader API, or related smart contracts, you
+            represent that you are not located in, incorporated or established in, or a citizen or resident of the
+            Prohibited Jurisdictions. You also represent that you are not subject to sanctions or otherwise designated
+            on any list of prohibited or restricted parties or excluded or denied persons, including but not limited to
+            the lists maintained by the United States' Department of Treasury's Office of Foreign Assets Control, the
+            United Nations Security Council, the European Union or its Member States, or any other government authority.
           </p>
         </div>
 
